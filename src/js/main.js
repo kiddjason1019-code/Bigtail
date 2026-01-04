571ac4d98330ee4469b94be8c584ef635e92de15
kiddjason1019-code <kiddjason1019@gmail.com>
2026-01-04 16:39:27 +0800

    Customize landing page for Xiaohai Jason

--- src/js/main.js
+++ src/js/main.js
@@ -39,12 +39,12 @@
     if (lightSwitch.checked) {
       body.classList.remove('lights-off')
       if (labelText) {
-        labelText.innerHTML = 'dark'
+        labelText.innerHTML = '夜間'
       }
     } else {
       body.classList.add('lights-off')
       if (labelText) {
-        labelText.innerHTML = 'light'
+        labelText.innerHTML = '日間'
       }
     }
   }
